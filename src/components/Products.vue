a418c8657d7432cab81070650cbe70ffa6d88c02
Ji Woong Park <jiwpark00@gmail.com>
2023-12-30 18:40:39 -0500

    updated Products.vue

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,5 +1,27 @@
 <script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: "1",
+          name: "airpods"
+        },
+        {
+          id: "2",
+          name: "kindle"
+        },
+        {
+          id: "3",
+          name: "keyboard"
+        }
+      ]
+    }
+  }
+}
 </script>
 <template>
-  <h1>Products</h1>
+  <div v-for='product in products':key='product.id'>
+    <h3>{{ product.id }} : {{ product.name }}</h3>
+  </div>
 </template>
