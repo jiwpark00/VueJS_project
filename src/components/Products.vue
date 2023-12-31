499cff2aa9dcde774d8bbe11eb5d4b9f9f6faad3
Ji Woong Park <jiwpark00@gmail.com>
2023-12-30 23:16:04 -0500

    Modified Products and Rating, and Created Product.vue

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,19 +1,36 @@
 <script>
+import Product from './Product.vue';
+
 export default {
+  components: {
+    Product
+  },
   data() {
     return {
       products: [
         {
           id: "1",
-          name: "airpods"
+          imageUrl: "http://loremflickr.com/150/150?random=1",
+          productName: "Product 1",
+          releasedDate: "May 31, 2016",
+          description: "Lorem ipsum dolor sit amet, consectetur..",
+          rating: 4
         },
         {
           id: "2",
-          name: "kindle"
+          imageUrl: "http://loremflickr.com/150/150?random=2",
+          productName: "Product 2",
+          releasedDate: "October 31, 2016",
+          description: "Lorem ipsum dolor sit amet, consectetur..",
+          rating: 2
         },
         {
           id: "3",
-          name: "keyboard"
+          imageUrl: "http://loremflickr.com/150/150?random=3",
+          productName: "Product 3",
+          releasedDate: "July 30, 2016",
+          description: "Lorem ipsum dolor sit amet, consectetur..",
+          rating: 5
         }
       ]
     }
@@ -21,7 +38,7 @@ export default {
 }
 </script>
 <template>
-  <div v-for='product in products':key='product.id'>
-    <h3>{{ product.id }} : {{ product.name }}</h3>
+  <div v-for='product in products' :key='product.id'>
+    <Product :data="product" />
   </div>
 </template>
--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -5,7 +5,8 @@ export default {
     return {
       // rating only uses this.initialRating as the initial value;
       // it is disconnected from future prop updates
-      rating: this.initialRating
+      rating: this.initialRating,
+      color: 'orange'
     }
   },
   methods: {
@@ -17,23 +18,23 @@ export default {
 </script>
 <template>
   <h1>Rating: {{ rating }}</h1>
-  <span v-on:click="assignRating(1)">
+  <span :style="{color:color}" v-on:click="assignRating(1)">
     <i v-if="rating >= 1" class="bi bi-star-fill"></i>
     <i v-else class="bi bi-star"></i>
   </span>
-  <span v-on:click="assignRating(2)">
+  <span :style="{color:color}" v-on:click="assignRating(2)">
     <i v-if="rating >= 2" class="bi bi-star-fill"></i>
     <i v-else class="bi bi-star"></i>
   </span>
-  <span v-on:click="assignRating(3)">
+  <span :style="{color:color}" v-on:click="assignRating(3)">
     <i v-if="rating >= 3" class="bi bi-star-fill"></i>
     <i v-else class="bi bi-star"></i>
   </span>
-  <span v-on:click="assignRating(4)">
+  <span :style="{color:color}" v-on:click="assignRating(4)">
     <i v-if="rating >= 4" class="bi bi-star-fill"></i>
     <i v-else class="bi bi-star"></i>
   </span>
-  <span v-on:click="assignRating(5)">
+  <span :style="{color:color}" v-on:click="assignRating(5)">
     <i v-if="rating >= 5" class="bi bi-star-fill"></i>
     <i v-else class="bi bi-star"></i>
   </span>
