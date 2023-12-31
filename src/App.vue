8179ba8c71e581c728f129c6b57908fcc5d94a82
Ji Woong Park <jiwpark00@gmail.com>
2023-12-30 19:04:14 -0500

    updated App.vue with Rating

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script>
-import Products from './components/Products.vue';
+import Rating from './components/Rating.vue';
 
 export default {
   components: {
-    Products
+    Rating
   }
 }
 </script>
 
 <template>
-  <Products />
-</template>
+  <Rating initialRating="1" />
+  <Rating initialRating="2" />
+  <Rating initialRating="3" />
+  <Rating initialRating="4" />
+  <Rating initialRating="5" />
+</template>``
